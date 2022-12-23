c8b5d561daccef07172a0698e00e7c7e7daaf97c
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2022-12-23 02:00:29 +0100

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"meander-pattern": "url('/meander.svg')",
+			},
+			colors: {
+				libyellow: "#F4D35E",
+				libgrey: "#393E41",
+				libsilver: "#C4CAD0",
+				libwhite: "#f9f7f4",
+			},
+		},
+	},
+
+	plugins: [require("daisyui")],
+
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#FCA17D",
+
+					secondary: "#000000",
+
+					accent: "#F7AEF8",
+
+					neutral: "#393E41",
+
+					"base-100": "#FFFFDD",
+
+					info: "#3F84E5",
+
+					success: "#00FFA3",
+
+					warning: "#F7C326",
+
+					error: "#ef4444",
+
+					"base-content": "#393E41",
+				},
+			},
+		],
+	},
+};
